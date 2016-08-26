0c2a4368fd853806a18be45cd9e0b924eff5360f
raychenfj <raychenfj@gmail.com>
2016-08-26 19:39:01 +0800

    Add e2e test
    Add e2e test

---
+++ protractor.conf.js
@@ -0,0 +1,24 @@
+//jshint strict: false
+exports.config = {
+
+  allScriptsTimeout: 11000,
+
+  specs: [
+    'test/**/e2e.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: 'http://localhost:8100/',
+
+  framework: 'jasmine',
+  useAllAngular2AppRoots: true,
+  // seleniumAddress: 'http://localhost:8100',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+
+};
