2c9389bb8390447b7135ed17b9adcc6e0cc54343
byzg <byzg@mail.ru>
2016-11-23 16:11:52 +0300

    alias

--- src/pages/advanced/advanced.ts
+++ src/pages/advanced/advanced.ts
@@ -14,7 +14,7 @@ export class AdvancedExamplePage {
 	default = '1 1-2 1-2-2';
 	dependentColumns: any[];
 	independentColumns: any[];
-	cityColumns: any[];
+  aliasColumns: any[];
 	fruits: any[];
 	fruit: Fruit;
 	Fruit;
@@ -81,5 +81,26 @@ export class AdvancedExamplePage {
 		this.fruit = Fruit.Melon;
 		this.Fruit = Fruit;
 		this.fruits = convertEnumToColumn(this.Fruit);
+
+    this.aliasColumns = [
+      {
+        parentCol: 'months',
+        options: [
+          { text: '1-1', value: '1-1', parentVal: '1' },
+          { text: '1-2', value: '2-1', parentVal: '1'},
+          { text: '1-3', value: '3-1', parentVal: '1' },
+
+          { text: '2-1', value: '1-2', parentVal: '2' },
+          { text: '2-2', value: '2-2', parentVal: '2' }
+        ]
+      },
+      {
+        alias: 'months',
+        options: [
+          { text: '1', value: '1' },
+          { text: '2', value: '2' }
+        ]
+      }
+    ]
 	}
 }
