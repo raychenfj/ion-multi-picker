eb58efd6ad4f5dac7c441a1acf0f56d5d5ac013b
陈逢隽 <raychenfj@gmail.com>
2016-11-27 17:19:25 +0800

    format

--- src/pages/advanced/advanced.ts
+++ src/pages/advanced/advanced.ts
@@ -14,12 +14,12 @@ export class AdvancedExamplePage {
 	default = '1 1-2 1-2-2';
 	dependentColumns: any[];
 	independentColumns: any[];
-  parentColumns: any[];
+	parentColumns: any[];
 	fruits: any[];
 	fruit: Fruit;
 	Fruit;
 	sepVal = '1_1 2_1 2 1';
-	sepColumns:any[];
+	sepColumns: any[];
 	separator = '_';
 
 
@@ -27,7 +27,7 @@ export class AdvancedExamplePage {
 		// Dependent columns
 		this.dependentColumns = [
 			{
-				columnWidth:'100px',
+				columnWidth: '100px',
 				options: [
 					{ text: '1', value: '1' },
 					{ text: '2', value: '2' },
@@ -36,12 +36,12 @@ export class AdvancedExamplePage {
 					{ text: '5', value: '5' },
 					{ text: '6', value: '6' },
 					{ text: '7', value: '7' },
-					{ text: '8', value: '8'},
-					{ text: '9', value: '9'},
-					{ text: '10', value: '10'}],
+					{ text: '8', value: '8' },
+					{ text: '9', value: '9' },
+					{ text: '10', value: '10' }],
 			},
 			{
-				columnWidth:'100px',
+				columnWidth: '100px',
 				options: [
 					{ text: '1-1', value: '1-1', parentVal: '1' },
 					{ text: '1-2', value: '1-2', parentVal: '1' },
@@ -62,39 +62,39 @@ export class AdvancedExamplePage {
 					{ text: '4-2', value: '4-2', parentVal: '4' },
 					{ text: '4-3', value: '4-3', parentVal: '4' },
 					{ text: '4-4', value: '4-4', parentVal: '4' },
-					
+
 					{ text: '5-1', value: '5-1', parentVal: '5' },
 					{ text: '5-2', value: '5-2', parentVal: '5' },
 					{ text: '5-3', value: '5-3', parentVal: '5' },
 					{ text: '5-4', value: '5-4', parentVal: '5' },
-					
+
 					{ text: '6-1', value: '6-1', parentVal: '6' },
 					{ text: '6-2', value: '6-2', parentVal: '6' },
 					{ text: '6-3', value: '6-3', parentVal: '6' },
 					{ text: '6-4', value: '6-4', parentVal: '6' },
-					
+
 					{ text: '7-1', value: '7-1', parentVal: '7' },
 					{ text: '7-2', value: '7-2', parentVal: '7' },
 					{ text: '7-3', value: '7-3', parentVal: '7' },
 					{ text: '7-4', value: '7-4', parentVal: '7' },
-					
+
 					{ text: '8-1', value: '8-1', parentVal: '8' },
 					{ text: '8-2', value: '8-2', parentVal: '8' },
 					{ text: '8-3', value: '8-3', parentVal: '8' },
 					{ text: '8-4', value: '8-4', parentVal: '8' },
-					
+
 					{ text: '9-1', value: '9-1', parentVal: '9' },
 					{ text: '9-2', value: '9-2', parentVal: '9' },
 					{ text: '9-3', value: '9-3', parentVal: '9' },
 					{ text: '9-4', value: '9-4', parentVal: '9' },
-					
+
 					{ text: '10-1', value: '10-1', parentVal: '10' },
 					{ text: '10-2', value: '10-2', parentVal: '10' },
 					{ text: '10-3', value: '10-3', parentVal: '10' },
 					{ text: '10-4', value: '10-4', parentVal: '10' }],
 			},
 			{
-				columnWidth:'100px',
+				columnWidth: '100px',
 				options: [
 					{ text: '1-1-1', value: '1-1-1', parentVal: '1-1' },
 					{ text: '1-1-2', value: '1-1-2', parentVal: '1-1' },
@@ -129,7 +129,7 @@ export class AdvancedExamplePage {
 					{ text: '2-4-2', value: '2-4-2', parentVal: '2-4' },
 					{ text: '2-4-3', value: '2-4-3', parentVal: '2-4' },
 					{ text: '2-4-4', value: '2-4-4', parentVal: '2-4' },
-					
+
 					{ text: '3-1-1', value: '3-1-1', parentVal: '3-1' },
 					{ text: '3-1-2', value: '3-1-2', parentVal: '3-1' },
 					{ text: '3-1-3', value: '3-1-3', parentVal: '3-1' },
@@ -146,7 +146,7 @@ export class AdvancedExamplePage {
 					{ text: '3-4-2', value: '3-4-2', parentVal: '3-4' },
 					{ text: '3-4-3', value: '3-4-3', parentVal: '3-4' },
 					{ text: '3-4-4', value: '3-4-4', parentVal: '3-4' },
-					
+
 					{ text: '4-1-1', value: '4-1-1', parentVal: '4-1' },
 					{ text: '4-1-2', value: '4-1-2', parentVal: '4-1' },
 					{ text: '4-1-3', value: '4-1-3', parentVal: '4-1' },
@@ -163,7 +163,7 @@ export class AdvancedExamplePage {
 					{ text: '4-4-2', value: '4-4-2', parentVal: '4-4' },
 					{ text: '4-4-3', value: '4-4-3', parentVal: '4-4' },
 					{ text: '4-4-4', value: '4-4-4', parentVal: '4-4' },
-					
+
 					{ text: '5-1-1', value: '5-1-1', parentVal: '5-1' },
 					{ text: '5-1-2', value: '5-1-2', parentVal: '5-1' },
 					{ text: '5-1-3', value: '5-1-3', parentVal: '5-1' },
@@ -180,7 +180,7 @@ export class AdvancedExamplePage {
 					{ text: '5-4-2', value: '5-4-2', parentVal: '5-4' },
 					{ text: '5-4-3', value: '5-4-3', parentVal: '5-4' },
 					{ text: '5-4-4', value: '5-4-4', parentVal: '5-4' },
-					
+
 					{ text: '6-1-1', value: '6-1-1', parentVal: '6-1' },
 					{ text: '6-1-2', value: '6-1-2', parentVal: '6-1' },
 					{ text: '6-1-3', value: '6-1-3', parentVal: '6-1' },
@@ -197,7 +197,7 @@ export class AdvancedExamplePage {
 					{ text: '6-4-2', value: '6-4-2', parentVal: '6-4' },
 					{ text: '6-4-3', value: '6-4-3', parentVal: '6-4' },
 					{ text: '6-4-4', value: '6-4-4', parentVal: '6-4' },
-					
+
 					{ text: '7-1-1', value: '7-1-1', parentVal: '7-1' },
 					{ text: '7-1-2', value: '7-1-2', parentVal: '7-1' },
 					{ text: '7-1-3', value: '7-1-3', parentVal: '7-1' },
@@ -214,7 +214,7 @@ export class AdvancedExamplePage {
 					{ text: '7-4-2', value: '7-4-2', parentVal: '7-4' },
 					{ text: '7-4-3', value: '7-4-3', parentVal: '7-4' },
 					{ text: '7-4-4', value: '7-4-4', parentVal: '7-4' },
-					
+
 					{ text: '8-1-1', value: '8-1-1', parentVal: '8-1' },
 					{ text: '8-1-2', value: '8-1-2', parentVal: '8-1' },
 					{ text: '8-1-3', value: '8-1-3', parentVal: '8-1' },
@@ -231,7 +231,7 @@ export class AdvancedExamplePage {
 					{ text: '8-4-2', value: '8-4-2', parentVal: '8-4' },
 					{ text: '8-4-3', value: '8-4-3', parentVal: '8-4' },
 					{ text: '8-4-4', value: '8-4-4', parentVal: '8-4' },
-					
+
 					{ text: '9-1-1', value: '9-1-1', parentVal: '9-1' },
 					{ text: '9-1-2', value: '9-1-2', parentVal: '9-1' },
 					{ text: '9-1-3', value: '9-1-3', parentVal: '9-1' },
@@ -248,7 +248,7 @@ export class AdvancedExamplePage {
 					{ text: '9-4-2', value: '9-4-2', parentVal: '9-4' },
 					{ text: '9-4-3', value: '9-4-3', parentVal: '9-4' },
 					{ text: '9-4-4', value: '9-4-4', parentVal: '9-4' },
-					
+
 					{ text: '10-1-1', value: '10-1-1', parentVal: '10-1' },
 					{ text: '10-1-2', value: '10-1-2', parentVal: '10-1' },
 					{ text: '10-1-3', value: '10-1-3', parentVal: '10-1' },
@@ -272,27 +272,27 @@ export class AdvancedExamplePage {
 		this.independentColumns = [
 			{
 				options: [{ text: '1', value: '1' },
-					{ text: '2', value: '2' },
-					{ text: '3', value: '3' }]
+				{ text: '2', value: '2' },
+				{ text: '3', value: '3' }]
 			},
 			{
 				options: [{ text: '1-1', value: '1-1' },
-					{ text: '1-2', value: '1-2' },
-					{ text: '2-1', value: '2-1' },
-					{ text: '2-2', value: '2-2' },
-					{ text: '3-1', value: '3-1' },]
+				{ text: '1-2', value: '1-2' },
+				{ text: '2-1', value: '2-1' },
+				{ text: '2-2', value: '2-2' },
+				{ text: '3-1', value: '3-1' },]
 			},
 			{
 				options: [{ text: '1-1-1', value: '1-1-1' },
-					{ text: '1-1-2', value: '1-1-2' },
-					{ text: '1-2-1', value: '1-2-1' },
-					{ text: '1-2-2', value: '1-2-2' },
-					{ text: '2-1-1', value: '2-1-1' },
-					{ text: '2-1-2', value: '2-1-2' },
-					{ text: '2-2-1', value: '2-2-1' },
-					{ text: '2-2-2', value: '2-2-2' },
-					{ text: '3-1-1', value: '3-1-1' },
-					{ text: '3-1-2', value: '3-1-2' },]
+				{ text: '1-1-2', value: '1-1-2' },
+				{ text: '1-2-1', value: '1-2-1' },
+				{ text: '1-2-2', value: '1-2-2' },
+				{ text: '2-1-1', value: '2-1-1' },
+				{ text: '2-1-2', value: '2-1-2' },
+				{ text: '2-2-1', value: '2-2-1' },
+				{ text: '2-2-2', value: '2-2-2' },
+				{ text: '3-1-1', value: '3-1-1' },
+				{ text: '3-1-2', value: '3-1-2' },]
 			}
 		];
 
@@ -302,39 +302,39 @@ export class AdvancedExamplePage {
 		this.fruits = convertEnumToColumn(this.Fruit);
 
 		// Using parentCol
-    this.parentColumns = [
-      {
+		this.parentColumns = [
+			{
 				name: 'child',
-        parentCol: 'parent',
-        options: [
-          { text: '1-1-1', value: '1-1-1', parentVal: '1-1' },
+				parentCol: 'parent',
+				options: [
+					{ text: '1-1-1', value: '1-1-1', parentVal: '1-1' },
 					{ text: '1-1-2', value: '1-1-2', parentVal: '1-1' },
-          { text: '1-2-1', value: '1-2-1', parentVal: '1-2' },
+					{ text: '1-2-1', value: '1-2-1', parentVal: '1-2' },
 					{ text: '1-2-2', value: '1-2-2', parentVal: '1-2' },
-          { text: '2-1-1', value: '2-1-1', parentVal: '2-1' },
+					{ text: '2-1-1', value: '2-1-1', parentVal: '2-1' },
 					{ text: '2-1-2', value: '2-1-2', parentVal: '2-1' },
-          { text: '2-2-1', value: '2-2-1', parentVal: '2-2' },
-          { text: '2-2-2', value: '2-2-2', parentVal: '2-2' }
-        ]
-      },
+					{ text: '2-2-1', value: '2-2-1', parentVal: '2-2' },
+					{ text: '2-2-2', value: '2-2-2', parentVal: '2-2' }
+				]
+			},
 			{
 				name: 'parent',
 				parentCol: 'ancestor',
-        options: [
-          { text: '1-1', value: '1-1', parentVal: '1' },
+				options: [
+					{ text: '1-1', value: '1-1', parentVal: '1' },
 					{ text: '1-2', value: '1-2', parentVal: '1' },
-          { text: '2-1', value: '2-1', parentVal: '2' },
+					{ text: '2-1', value: '2-1', parentVal: '2' },
 					{ text: '2-2', value: '2-2', parentVal: '2' },
-        ]
-      },
-      {
+				]
+			},
+			{
 				name: 'ancestor',
-        options: [
-          { text: '1', value: '1' },
-          { text: '2', value: '2' }
-        ]
-      }
-    ];
+				options: [
+					{ text: '1', value: '1' },
+					{ text: '2', value: '2' }
+				]
+			}
+		];
 
 		// Using custom separator and the value contains space 
 		this.sepColumns = [
