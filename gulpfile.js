e8a53fa28059e5c59f497a140047c8bb41ec962a
陈逢隽 <raychenfj@gmail.com>
2017-07-29 12:25:53 +0800

    add prepare script

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,15 @@ var gulp = require('gulp'),
     runSequence = require('run-sequence'),
     argv = process.argv;
 
-gulp.task('del', function() {
+gulp.task('del', function () {
     return del(['dist', '*.scss', '*.tgz']);
 });
 
-gulp.task('copyhtml',function(){
+gulp.task('copyhtml', function () {
     gulp.src('src/components/multi-picker/*.html').pipe(gulp.dest('dist/components/multi-picker'));
 })
 
-gulp.task('copyscss', function() {
+gulp.task('copyscss', function () {
     gulp.src('src/components/multi-picker/*.scss').pipe(gulp.dest('dist/components/multi-picker'));
 });
 
@@ -23,13 +23,17 @@ var shell = require('gulp-shell');
 gulp.task('ngc', shell.task(['ngc -p ./config/tsconfig.build.json']));
 gulp.task('pack', shell.task(['npm pack']));
 
-gulp.task('post', function(){
+gulp.task('post', function () {
     return del(['./config/node_modules', './config/src']);
 });
 
 
-gulp.task('default', function() {
-    runSequence('del','copyhtml', 'copyscss', 'ngc', 'pack','post')
+gulp.task('pack', function () {
+    runSequence('del', 'copyhtml', 'copyscss', 'ngc', 'pack', 'post')
 });
+
+gulp.task('default', function () {
+    runSequence('del', 'copyhtml', 'copyscss', 'ngc', 'post')
+})
 
 
